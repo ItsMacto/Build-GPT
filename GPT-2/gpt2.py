c562635c6fbd3b3679aa855f5cdf179382a269e5
Mac Howe <mac@machowe.com>
2025-04-28 23:20:01 -0400

    MLP Block added

--- GPT-2/gpt2.py
+++ GPT-2/gpt2.py
@@ -4,6 +4,21 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
+class MLP(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh') #used to be faster but now have to use for loading compatibility for gpt 2
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
 class Block(nn.Module):
     
     def __init__(self, config):
